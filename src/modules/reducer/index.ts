b5e1f1018619bf2802d7cdec317d1d96b4710f1c
shc0801 <shc123987@naver.com>
2021-11-10 11:31:57 +0900

    feat: add reducer page

---
+++ src/modules/reducer/index.ts
@@ -0,0 +1,13 @@
+import { combineReducers } from "redux";
+import { Page } from "../actions";
+import page from "./page";
+
+export type RootState = {
+    page: Page,
+}
+
+const rootReducer = combineReducers({
+    page: page
+});
+
+export default rootReducer;
