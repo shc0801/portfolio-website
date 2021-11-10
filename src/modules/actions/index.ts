53d0dc1379c6bc8b71aeff1f6f051007097e625f
shc0801 <shc123987@naver.com>
2021-11-10 11:30:51 +0900

    feat: redux module action page

---
+++ src/modules/actions/index.ts
@@ -0,0 +1,14 @@
+import { ActionType, deprecated } from "typesafe-actions";
+
+const { createStandardAction } = deprecated;
+
+export const PAGE_MOVE = "page/PAGE_MOVE";
+
+export const pageMove = createStandardAction(PAGE_MOVE)<{
+  page: string
+}>();
+
+export type PageAction = ActionType<typeof pageMove>
+export type Page = {
+  page: string
+}
